5ebf6edb6e375154af6fa6bf58ee3b2530e31004
Marcos676 <patino-marcos@outlook.com>
2026-06-22 09:59:43 -0300

    Ordena  elementos de componentes principales

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -4,12 +4,18 @@ import InputsDashboard from "./InputsDashboard";
 import ProstheticHandView from "./ProstheticHandView";
 
 function App() {
-
+  // revisar luego. Para adaptarlo a la distribucion con grid que vi en el tutorial de grid superponiendo elementos. (casi final del video creo)
   return (
-    <div className="App flex items-center justify-center h-screen bg-gray-100">
-      <BiomedicDashboard/>
-      <ProstheticHandView/>
-      <InputsDashboard/>
+    <div className="App flex items-center justify-between h-screen w-screen bg-gray-100 relative">
+      <div className="">
+        <BiomedicDashboard />
+      </div>
+      <div className="">
+        <ProstheticHandView />
+      </div>
+      <div className="">
+        <InputsDashboard />
+      </div>
     </div>
   );
 }
